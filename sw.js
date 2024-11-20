8e77f530872547f3cc573a8abd19064caa0a5d07
lqjava <545209147@qq.com>
2024-11-20 22:42:02 +0800

    Site updated: 2024-11-20 22:42:02

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732113650820";
+const VERSION = "1732113722026";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
